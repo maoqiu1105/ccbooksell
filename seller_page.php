7e4343dd1e6bd8db264ee9236d3189f60bda4fff
maoqiu1105 <wangqiao921105@gmail.com>
2017-11-20 14:49:35 -0500

    qiao

--- seller_page.php
+++ seller_page.php
@@ -3,42 +3,55 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="./css_folder/book_list.css">
+	<link rel="stylesheet" type="text/css" href="./css_folder/seller_page.css">
 	<link rel="stylesheet" type="text/css" href="./css_folder/shared.css">
 	<script src="checkdata.js"></script>
 	<title>seller profile</title>
 </head>
 <body>
 
+	<?php
+	include('login_process.php');
+	while($row = $result -> fetch_assoc())
+		{
+			$The_seller_ID = $row['seller_ID'];
+			$book_result = "SELECT book_Name,book_Price,date_Post,book_ISBN,book_ID,category_Name FROM tblcategory
+								JOIN  tblbook 
+									ON tblcategory.category_ID = tblbook.book_Category_ID
+								JOIN tblseller
+									ON tblbook.seller_ID=tblseller.seller_ID
+								WHERE tblseller.seller_ID = '".$The_seller_ID."'";
+			$book_result = $conn->query($book_result);
+			
+			echo
+			"<p>Welcome, ".$row['seller_Name']."<br><br></p>";
+				
+			//fetch the data from database and display them by table	
+			if($book_result->num_rows > 0 )
+			{
+				while($book_row = $book_result -> fetch_assoc())
+				{
+					echo
+					"<table>
+						<tr><td>Book Name : </td><td>".$book_row['book_Name']."</td></tr>
+						<tr><td>Book category : </td><td>".$book_row['category_Name']."</td></tr>
+						<tr><td>Book ISBN : </td><td>".$book_row['book_ISBN']."</td></tr>
+						<tr><td>Book Price : </td><td>".$book_row['book_Price']."</td></tr>
+						<tr><td>Post date : </td><td>".$book_row['date_Post']."</td></tr>
+					</table>";
+				}
+				
+			}
+			else {
+				echo "your don't have any book here, do you want to add book?";
+			}
+		}
+	?>
+	<!-- line to divide the footer  -->
+	<hr>
+	<footer>
+		<a href="index.html">Home</a>
+	</footer>
 </body>
 </html>
-<?php
-include('login_process.php');
-while($row = $result -> fetch_assoc())
-	{
-		$The_seller_ID = $row['seller_ID'];
-		$book_result = "SELECT * FROM tblbook WHERE seller_ID = '".$The_seller_ID."'";
-		$book_result = $conn->query($book_result);
-		
-		echo
-		"<p>Welcome, ".$row['seller_Name']."<br><br></p>";
-			
-		if($book_result->num_rows > 0 )
-		{
-			while($book_row = $book_result -> fetch_assoc())
-			{
-				echo
-					"Book category : ".$book_row['book_Category_ID']."<br/>".
-					"Book ID : ".$book_row['book_ID']."<br/>".
-					"Book ISBN : ".$book_row['book_ISBN']."<br/>".
-					"Book Name : ".$book_row['book_Name']."<br/>".
-					"Book Price : ".$book_row['book_Price']."<br/>".
-					"Sold Book : ".$book_row['book_Sold']."<br/>".
-					"Post date : ".$book_row['date_Post']."<br/>".
-					"Seller ID : ".$book_row['seller_ID'].
-					"<br><br><br><br>";
-			}
-			
-		}
-	}
-?>
+
